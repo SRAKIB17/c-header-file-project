817aaef0f6e410bbbb45c0b6be45d041e700deef
SRAKIB17 <rakibulssc5@gmail.com>
2023-08-03 13:55:42 +0600

    'init

---
+++ myheader.h
@@ -0,0 +1,11 @@
+#ifndef MYHEADER_H // Header guard to prevent multiple inclusion
+#define MYHEADER_H
+
+// Function prototypes
+int add(int a, int b);
+void sayHello();
+
+// Macro definitions
+#define PI 3.14159
+
+#endif // MYHEADER_H
---
+++ test.c
@@ -0,0 +1,11 @@
+#include "myheader.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+void sayHello()
+{
+    printf("Hello, world!\n");
+}
